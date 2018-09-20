6c5916178e8c9f42ed9ea59bbdbb130a03c2d610
sertoi <nguyentuongvien@yahoo.com>
2018-09-20 10:18:43 +0200

    Ajouter Collumn et Table class AgenceVoyage

--- WebBoVoyage/Models/AgenceVoyage.cs
+++ WebBoVoyage/Models/AgenceVoyage.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace WebBoVoyage.Models
 {   [Table ("AgenceVoyage")]
     public class AgenceVoyage
     {
-     
 
-            public int Id { get; set; }
-            public string Nom { get; set; }
+        [Column("Id")]
+        public int Id { get; set; }
+        [Column("Nom")]
+        [StringLength(50)]
+        public string Nom { get; set; }
             //[NotMapped]
             //public ICollection<Voyage> Voyages {get;set;}
 
