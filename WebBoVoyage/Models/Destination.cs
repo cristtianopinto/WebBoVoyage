4823b349c65b970c987a810c63acd6118e55b4f9
sertoi <nguyentuongvien@yahoo.com>
2018-09-20 10:39:31 +0200

    Ajouter class Destination dans Models

---
+++ WebBoVoyage/Models/Destination.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebBoVoyage.Models
+{
+    [Table("Destination")]
+    public class Destination
+    {
+        [Column("Id")]
+        public int Id { get; set; }
+        [Column("Continent")]
+        public string Continent { get; set; }
+        [Column("Pays")]
+        public string Pays { get; set; }
+        [Column("Region")]
+        public string Region { get; set; }
+        [Column("Description")]
+        public string Description { get; set; }
+    }
+}
